84d15d98586f9d97507a868942df566a3eb8e112
Dipendra <dipendrarajpanta1@gmail.com>
2020-06-27 12:25:49 +0545

    Ready for deployment

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import os
 from datetime import datetime
-from flask import Flask,render_template, request,session, redirect, url_for
+from flask import Flask,render_template, request,session, redirect, url_for, jsonify, abort
 from flask_socketio import SocketIO, emit
+from flask_session import Session
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
+
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
@@ -18,9 +24,13 @@ def index():
         if request.form['username']=='':
             return redirect(url_for('index'))
         session['username'] = request.form['username']
+        session['channel'] = 'general'
         usernames.append(session['username'])
-        print(usernames)
+        #print(usernames)
         return redirect(url_for('index'))
+    if "username" in session:
+        if session['channel']:
+            return redirect(url_for('channel',channelName=session['channel']))
     return render_template('index.html', chatrooms = chatroom, active = 'general')
 
 @app.route('/logout')
@@ -28,12 +38,18 @@ def logout():
     session.clear()
     return redirect(url_for('index'))
 
-@app.route('/channel/<channelName>')
+@app.route('/channel/<channelName>', methods=['GET', 'POST'])
 def channel(channelName):
-    print(messagedict)
+    if channelName not in chatroom:
+        abort(404)
     session['channel'] = channelName
     return render_template('index.html', chatrooms = chatroom, active = channelName)
     
+@app.route('/listmessages', methods=['POST'])
+def listmessages():
+    return jsonify({"message":messagedict[session['channel']],"channel":session['channel']})
+    
+
 @socketio.on('sendMessage')
 def message(data):
     message = data['message']
@@ -43,13 +59,19 @@ def message(data):
         user = user, time= time, message = message
     )
     channel = session['channel']
-    messagedict[channel].append(msg)
-    print(messagedict)
+    messagelist = messagedict[channel]
+    if len(messagelist) == 100:
+        del messagelist[0]
+    messagelist.append(msg)
+    #print(messagedict)
     emit('received message', {"message": message,"time": time, "user": user}, broadcast=True)
 
 @socketio.on('createChannel')
 def channelcreate(data):
     channel = data['name']
-    chatroom.append(channel)
-    messagedict[channel] = []
-    emit('channel create', {"channelname":channel})
+    if channel in chatroom:
+        emit('error create',{"message":"Channel name already exists."})
+    else:
+        chatroom.append(channel)
+        messagedict[channel] = []
+        emit('channel create', {"channelname":channel})
--- static/js/chat.js
+++ static/js/chat.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    const request = new XMLHttpRequest();
+    request.open("POST", "/listmessages");
+
+    // Callback function for when request completes
+    request.onload = () => {
+        const data = JSON.parse(request.responseText);
+        let mesg = data["message"];
+        for (var index in mesg) {
+            var template = Handlebars.templates.message;
+            var msg = template({ message: mesg[index].message, author: mesg[index].user, time: mesg[index].time })
+            document.querySelector('#chatBox').insertAdjacentHTML('beforeend', msg);
+        }
+    };
+    request.send();
+
     var element = document.getElementById("chatBox");
     element.scrollTop = element.scrollHeight;
     $("#sidebar").mCustomScrollbar({
@@ -17,17 +33,26 @@ document.addEventListener('DOMContentLoaded', () => {
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port );
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
     function sendMsg() {
         var message = document.getElementById("messageId").value;
-        if(message.length == 0){
+        if (message.length == 0) {
             return false;
         }
-        socket.emit('sendMessage',{message});
+        socket.emit('sendMessage', { message });
         document.getElementById("messageId").value = null;
     };
-    
-    
+
+    function create() {
+        var name = document.getElementById("channelName").value;
+        if (name.length == 0) {
+            return false;
+        }
+        socket.emit('createChannel', { name });
+        document.getElementById("channelName").value = null;
+    };
+
+
     socket.on('connect', () => {
         sendMessage.onclick = sendMsg;
         var send = document.getElementById("messageId");
@@ -36,19 +61,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 sendMsg();
             }
         });
-        channelCreate.onclick = function () {
-            var name = document.getElementById("channelName").value;
-            if(name.length == 0){
-                return false;
+        channelCreate.onclick = create;
+        var channel = document.getElementById("channelName");
+        channel.addEventListener('keydown', function onEvent(event) {
+            if (event.key === "Enter") {
+                create();
             }
-            socket.emit('createChannel',{name});
-            document.getElementById("channelName").value = null;
-        };
+        });
     });
 
     socket.on('received message', data => {
         var template = Handlebars.templates.message;
-        var msg = template({message: data.message,author: data.user,time: data.time})
+        var msg = template({ message: data.message, author: data.user, time: data.time })
         document.querySelector('#chatBox').insertAdjacentHTML('beforeend', msg);
         var element = document.getElementById("chatBox");
         element.scrollTop = element.scrollHeight;
@@ -59,7 +83,16 @@ document.addEventListener('DOMContentLoaded', () => {
         li.innerHTML = `<a href="/channel/${data.channelname}">#${data.channelname}</a>`
         document.querySelector('#channelList').append(li);
     });
-       
+
+    socket.on('error create', data =>{
+        var div = document.createElement("div");
+        div.setAttribute('class','alert alert-danger');
+        div.innerText = data.message;
+        let el = document.getElementById('channelForm');
+        let parentdiv = el.parentNode;
+        parentdiv.insertBefore(div, el.nextSibling)
+    });
+
 });
 
 
