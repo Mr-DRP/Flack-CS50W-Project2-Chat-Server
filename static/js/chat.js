1039a2c5e17a23c320e2c47a4517cc41dec85370
Dipendra <dipendrarajpanta1@gmail.com>
2020-06-27 16:01:20 +0545

    gunicorn added in Procfile

--- static/js/chat.js
+++ static/js/chat.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 
-    var socket = io.connect();
+    
     function sendMsg() {
         var message = document.getElementById("messageId").value;
         if (message.length == 0) {
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById("channelName").value = null;
     };
 
-
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
     socket.on('connect', () => {
         sendMessage.onclick = sendMsg;
         var send = document.getElementById("messageId");
